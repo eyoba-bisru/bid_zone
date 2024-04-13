1d5baf305edb71611b4d2f35cd1ce533118fad87
Eyob <eabebe91@gmail.com>
2024-04-13 09:05:48 +0300

    product card added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 import ScrollToTop from "@/components/ScrollToTop";
 import Navbar from "@/components/Navbar";
 import FilterByCategory from "@/components/FilterByCategory";
+import ProductCard from "@/components/ProductCard";
 
 export default function Home() {
   return (
@@ -11,10 +12,19 @@ export default function Home() {
 
       <FilterByCategory />
 
-      <div className="flex justify-center items-center">
+      <div className="flex justify-center items-center mb-10">
         <h1 className="text-3xl font-bold">
           Discover amazing deals through bidding
         </h1>
+      </div>
+
+      <div className="flex flex-wrap gap-4 justify-center items-center">
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
       </div>
     </div>
   );
