3753ec57d82ecabd36d79a3f396bc567d21a1759
Eyob <eabebe91@gmail.com>
2024-04-18 15:00:13 +0300

    fix: all category doesn't search

--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -5,39 +5,65 @@ export async function GET(request: Request) {
   const category = param.get("category");
   const q = param.get("q");
 
+  if (category != "null" && category != "all") {
+    const res = await prisma.product.findMany({
+      where: {
+        AND: [
+          {
+            category: {
+              name: category as string,
+            },
+          },
+          {
+            OR: [
+              {
+                title: {
+                  contains: q as string,
+                  mode: "insensitive",
+                },
+              },
+              {
+                descrition: {
+                  contains: q as string,
+                  mode: "insensitive",
+                },
+              },
+            ],
+          },
+        ],
+      },
+      include: {
+        category: true,
+        condition: true,
+      },
+    });
+
+    return Response.json(res);
+  }
+
   const res = await prisma.product.findMany({
     where: {
-      AND: [
+      OR: [
         {
-          category: {
-            name: category as string,
+          title: {
+            contains: q as string,
+            mode: "insensitive",
           },
         },
         {
-          OR: [
-            {
-              title: {
-                contains: q as string,
-                mode: "insensitive",
-              },
-            },
-            {
-              descrition: {
-                contains: q as string,
-                mode: "insensitive",
-              },
-            },
-          ],
+          descrition: {
+            contains: q as string,
+            mode: "insensitive",
+          },
         },
       ],
     },
+
     include: {
       category: true,
       condition: true,
     },
   });
-
-  console.log(res);
 
   return Response.json(res);
 }
